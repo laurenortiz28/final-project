44bf1d3dbbc0ac1f020bc58b8223785f867f2e8f
laurenortiz28 <lortiz28@asu.edu>
2023-09-07 21:04:23 -0500

    changes
    turned shit id into saying morning or noon and will code later into java to display the time so it will be easier

--- EMP_INFO.sql
+++ EMP_INFO.sql
@@ -5,7 +5,8 @@ Create Table employee_information (
         phone_number VARCHAR(15),
         employee_id INT,
         cafe_id INT,
-        shift_id INT,
+        shift_id VARCHAR(30),
         email VARCHAR(50),
+        job_pos VARCHAR(30), 
         CONSTRAINT PK_employee_information PRIMARY KEY (employee_id)
 );
