03f1c83f912480468490baa627ec167b7927c118
michaelquinta80 <143906980+michaelquinta80@users.noreply.github.com>
2023-09-21 18:20:59 -0600

    Add files via upload

---
+++ create_database.py
@@ -0,0 +1,18 @@
+import sqlite3
+conn = sqlite3.connect('employeedata.db')
+c=conn.cursor()
+
+c.execute("""CREATE TABLE employee(
+          employee_id INTEGER PRIMARY KEY,
+          first_name TEXT,
+          last_name TEXT,
+          phone_number INTEGER,
+          cafe_id INTEGER,
+          shift_id TEXT,
+          email TEXT,
+          job_pos TEXT
+)""")
+
+conn.commit()
+conn.close()
+
