2a788cf638177a19a71ba8677125271819eecf7c
bghenry23 <145713203+bghenry23@users.noreply.github.com>
2023-10-03 08:39:20 -0700

    Add files via upload

---
+++ schedules_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `schedules` (
+  `id` int NOT NULL,
+  `employee_id` int NOT NULL,
+  `Cafe_id` int NOT NULL,
+  `schedule_id` int NOT NULL,
+  `shift_id` varchar(25) NOT NULL,
+  `workdate` varchar(45) NOT NULL,
+  `hours_worked` int NOT NULL,
+  `shift_availability` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `employee_id_idx` (`employee_id`),
+  CONSTRAINT `employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee_information` (`employee_id`)
+);
