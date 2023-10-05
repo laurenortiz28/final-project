70088e25ff308ce8241343e0583cc49e46ed2868
bghenry23 <145713203+bghenry23@users.noreply.github.com>
2023-10-04 18:43:06 -0700

    Update shifts_table.sql

--- Henry_DB/shifts_table.sql
+++ Henry_DB/shifts_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE shifts (
   id int NOT NULL,
-  cafe_id int DEFAULT NULL,
+  cafe_id int NOT NULL,
   employee_id int NOT NULL,
   shift_id varchar(45) NOT NULL,
   workday varchar(45) NOT NULL,
